3dd03860ce61af528edbcc11d5f3b8cc979c51ba
Bernardo Albuquerque <albuquerque.sys@gmail.com>
2025-02-27 23:03:12 -0300

    Criadas as rotas do CRUD do projeto.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\TestamentoController;
+use App\Http\Controllers\LivroController;
+use App\Http\Controllers\VersiculoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/testamento', [TestamentoController::class, 'index']);
+Route::get('/testamento/{id}', [TestamentoController::class, 'show']);
+Route::put('/testamento/{id}', [TestamentoController::class, 'update']);
+Route::post('/testamento', [TestamentoController::class, 'store']);
+Route::delete('/testamento/{id}', [TestamentoController::class, 'destroy']);
+
+Route::get('/livro', [LivroController::class, 'index']);
+Route::get('/livro/{id}', [LivroController::class, 'show']);
+Route::put('/livro/{id}', [LivroController::class, 'update']);
+Route::post('/livro', [LivroController::class, 'store']);
+Route::delete('/livro/{id}', [LivroController::class, 'destroy']);
+
+Route::get('/versiculo', [VersiculoController::class, 'index']);
+Route::get('/versiculo/{id}', [VersiculoController::class, 'show']);
+Route::put('/versiculo/{id}', [VersiculoController::class, 'update']);
+Route::post('/versiculo', [VersiculoController::class, 'store']);
+Route::delete('/versiculo/{id}', [VersiculoController::class, 'destroy']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
